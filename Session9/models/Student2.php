9c7770ff28c887f99bd2d86a07d721bfed72de27
Mohamed Ghanem <mohamedyehia1998@gmail.com>
2020-08-29 19:10:06 +0200

    added alternate student

---
+++ Session9/models/Student2.php
@@ -0,0 +1,114 @@
+<?php
+
+# "." means in the same directory, another "." means one level outside ... etc
+require_once('../config/dbConnect.php');
+
+class Student{
+
+    private $id;
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $phone;
+    private $education;
+    private $occupation;
+
+    # interface between student and database
+    private static $connection;
+
+
+    public function get_id(){
+        return $this->id;
+    }
+
+    public function get_firstName(){
+        return $this->firstName;
+    }
+    
+    public function get_lastName(){
+        return $this->lastName;
+    }
+    
+    public function get_email(){
+        return $this->email;
+    }
+    
+    public function get_phone(){
+        return $this->firstName;
+    }
+    
+    public function get_education(){
+        return $this->lastName;
+    }
+    
+    public function get_occupation(){
+        return $this->email;
+    }
+    #####
+
+    public function set_firstName($firstName){
+        $this->firstName = $firstName;
+    }
+    
+
+    public function set_lastName($lastName){
+        $this->lastName = $lastName;
+    }
+    
+    public function set_email($email){
+        $this->email = $email;
+    }
+    
+    public function set_phone($phone){
+        $this->phone = $phone;
+    }
+    
+    public function set_education($education){
+        $this->education = $education;
+    }
+    
+    public function set_occupation($occupation){
+        $this->occupation = $occupation;
+    }
+
+
+    public function add_student(){
+        # connect with database
+        global $connection;   # use the connection variable in the dbConnect file inside this function (hence the word global).
+        self::$connection = $connection; # now connect the student object with the mysql database
+
+        $firstName = $this->get_firstName();
+        $lastName = $this->get_lastName();
+        $education = $this->get_education();
+        $email = $this->get_email();
+        $phone = $this->get_phone();
+        $occupation = $this->get_occupation();
+
+        $sql = "INSERT INTO students (first_name, last_name, education, email, phone, occupation) 
+        VALUES ($firstName, $lastName, $education, $email, $phone, $occupation)"; 
+
+
+        $prepared_sql = self::$connection->prepare($sql);
+        $prepared_sql->execute();
+
+        #return the newly entered row id
+        return self::$connection->lastInsertId();
+
+    }
+    
+    public static function all_students(){
+        # connect with database
+        global $connection;   # use the connection variable in the dbConnect file inside this function (hence the word global).
+        self::$connection = $connection; # now connect the student object with the mysql database
+
+
+        $sql = "SELECT * FROM students"; 
+
+        $prepared_sql = self::$connection->prepare($sql);
+        $prepared_sql->execute();
+
+        #return ALL present rows
+        return $prepared_sql->fetchAll(PDO::FETCH_OBJ);
+
+    }
+}
